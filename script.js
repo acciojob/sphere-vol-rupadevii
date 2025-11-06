7471a1d982dbc0fbedd87f8b02a44410fc998efc
Rupa Devi <rupadevi7094@gmail.com>
2025-11-06 10:53:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function volume_sphere() {
+function volume_sphere(e) {
     //Write your code here
+	e.preventDefault()
+	const radius = document.querySelector('input[name="radius"]').value;
+	const volumeInput = document.querySelector('input[name="volume"]');
+	
+	if(Number(radius) > 0){
+		const volume = (4/3) * Math.PI * radius * radius * radius;
+		volumeInput.value=volume.toFixed(4)
+	}
+	else{
+		volumeInput.value = NaN
+	}
   
 } 
 
